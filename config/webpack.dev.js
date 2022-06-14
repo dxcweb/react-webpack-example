25e932f775cd76b46ac6e79c61c604e03a91c45d
郭威 <dxcweb2017@gmail.com>
2022-06-14 16:35:52 +0800

    no message

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,13 +5,22 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "development",
-
   stats: "minimal",
   //   devtool: 'source-map',
   devtool: "cheap-module-source-map",
-
   devServer: {
+    historyApiFallback: true,
     allowedHosts: "all",
     port: 8888,
+    proxy: {
+      "/api": {
+        target: "https://xxxx.com",
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" },
+        headers: {
+          Referer: "https://xxxx.com",
+        },
+      },
+    },
   },
 });
